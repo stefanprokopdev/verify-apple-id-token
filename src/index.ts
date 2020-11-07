6a65721a793ddc2059a39f99e4bcad2765203a63
Štefan Prokop <sprokop1010@seznam.cz>
2020-11-07 13:35:14 +0100

    ✨ Update verification steps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { default as verifyToken, getApplePublicKey } from './verifyAppleIdToken';
+import { default as verifyToken, getApplePublicKey, VerifyAppleIdTokenParams } from './lib/verifyAppleIdToken';
 
 export default verifyToken;
 export {
     getApplePublicKey,
+    VerifyAppleIdTokenParams,
 };
--- src/lib/verifyAppleIdToken.ts
+++ src/lib/verifyAppleIdToken.ts
@@ -2,12 +2,18 @@ import * as jwt from 'jsonwebtoken';
 import * as jwksClient from 'jwks-rsa';
 
 export const APPLE_BASE_URL = 'https://appleid.apple.com';
-export const JWKS_URI = '/auth/keys'
+export const JWKS_APPLE_URI = '/auth/keys';
+
+export interface VerifyAppleIdTokenParams {
+    idToken: string;
+    clientId: string;
+    nonce?: string;
+}
 
 export const getApplePublicKey = async (kid) => {
     const client = jwksClient({
         cache: true,
-        jwksUri: `${APPLE_BASE_URL}${JWKS_URI}`,
+        jwksUri: `${APPLE_BASE_URL}${JWKS_APPLE_URI}`,
     });
     const key: any = await new Promise((resolve, reject) => {
         client.getSigningKey(kid, (error, result) => {
@@ -20,16 +26,19 @@ export const getApplePublicKey = async (kid) => {
     return key.publicKey as jwksClient.CertSigningKey || key.rsaPublicKey as jwksClient.RsaSigningKey;
 };
 
-export default async (idToken: string, clientId?: string) => {
-    const decoded = jwt.decode(idToken, { complete: true });
+export default async (params: VerifyAppleIdTokenParams) => {
+    const decoded = jwt.decode(params.idToken, { complete: true });
     const { kid, alg } = decoded.header;
     const applePublicKey = await getApplePublicKey(kid);
-    const jwtClaims = jwt.verify(idToken, applePublicKey, { algorithms: [alg] });
+    const jwtClaims = jwt.verify(params.idToken, applePublicKey, {
+        algorithms: [alg],
+        nonce: params.nonce,
+    });
     if (!jwtClaims.iss || jwtClaims.iss !== APPLE_BASE_URL) {
         throw new Error(`The iss does not match the Apple URL - iss: ${jwtClaims.iss} | expected: ${APPLE_BASE_URL}`);
     }
-    if (clientId && jwtClaims.aud !== clientId) {
-        throw new Error(`The aud parameter does not include this client - is: ${jwtClaims.aud} | expected: ${clientId}`);
+    if (jwtClaims.aud !== params.clientId) {
+        throw new Error(`The aud parameter does not include this client - is: ${jwtClaims.aud} | expected: ${params.clientId}`);
     }
     return jwtClaims;
 };
