82252f3dca6d7852f048f21161a776f4c7ee462e
Luis Arias <luis@upskyld.com>
2023-06-27 11:40:44 +0200

    Update src/lib/verifyAppleIdToken.ts
    Best practice not to log nonce values
    
    Co-authored-by: Štefan Prokop <sprokop1010@seznam.cz>

--- src/lib/verifyAppleIdToken.ts
+++ src/lib/verifyAppleIdToken.ts
@@ -21,7 +21,7 @@ export const verifyToken = async (params: VerifyAppleIdTokenParams) => {
   const { payload: jwtClaims } = await jwtVerify(params.idToken, applePublicKey);
 
   if ((jwtClaims?.nonce ?? undefined) !== params.nonce) {
-    throw new Error(`The nonce parameter does not match - nonce: ${jwtClaims.nonce} | expected: ${params.nonce}`);
+    throw new Error(`The nonce parameter does not match`);
   }
 
   if (jwtClaims?.iss !== APPLE_BASE_URL) {
